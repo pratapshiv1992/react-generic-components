c0023ce4d89e1e9dfffafc02fe12ef9fa03e4bba
shiv pratap singh <pratapshiv1992@gmail.com>
2019-10-04 23:11:41 +0530

    handlers implemented using props and other refactoring

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -1,21 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import '../styles/index.css';
 
-export const ChipButton = ({name, label, onClick, children, disabled}) => {
-    const [eventTransitionClass, setEventTransitionClass] = useState("chip-button-hover-out");
+export const ChipButton = ({name, label, onClick, children, disabled, onMouseOver, onMouseOut}) => {
     return (
-        <React.Fragment>
-            <button name={name} className={`chip-button ${eventTransitionClass}`} disabled={disabled} onClick={(e) => {
-                setEventTransitionClass("chip-button-pressed");
-                setTimeout(() => {
-                    setEventTransitionClass("chip-button-hover-out");
-                }, 200);
-                if (typeof onClick === "function") {
-                    onClick(e);
-                }
-            }} onMouseOver={() => setEventTransitionClass("chip-button-hover-in")}
-                    onMouseOut={() => setEventTransitionClass("chip-button-hover-out")}><span
-                className="chip-button-label">{label}</span>{children}</button>
-        </React.Fragment>
+        <button
+            name={name} className="chip-button" disabled={disabled}
+            onClick={onClick} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
+            <span className="chip-button-label">{label}</span>{children}
+        </button>
     );
 }
