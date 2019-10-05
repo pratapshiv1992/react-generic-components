dc7575bad67c3eed27b07ad798a869e575cc2df8
shiv pratap singh <pratapshiv1992@gmail.com>
2019-10-05 11:18:03 +0530

    api utility function implemented

--- src/App.js
+++ src/App.js
@@ -1,64 +1,16 @@
 import React from 'react';
 import ItemCard from './common/IteamCard';
-import Aenean_Sm from './assets/Aenean_Sm.png';
+import {callApi} from './utils';
 import './styles/index.css';
 
-const ItemCardData = [
-    [{
-        src: Aenean_Sm,
-        alt: "",
-        width: 240,
-        description: " Duis rhoncus dui venenatis consequat porttitor",
-        title: "Aenean Sed Nibh A Magna Posuere",
-        link: "/",
-        linkText: "Link",
-        price: "$4.99"
-    }, {
-        src: Aenean_Sm,
-        alt: "",
-        width: 240,
-        description: " Duis rhoncus dui venenatis consequat porttitor",
-        title: "Aenean Sed Nibh A Magna Posuere",
-        link: "/",
-        linkText: "Link",
-        price: "$4.99"
-    }, {
-        src: Aenean_Sm,
-        alt: "",
-        width: 240,
-        description: " Duis rhoncus dui venenatis consequat porttitor",
-        title: "Aenean Sed Nibh A Magna Posuere",
-        link: "/",
-        linkText: "Link",
-        price: "$4.99"
-    }],
-    [{
-        src: Aenean_Sm,
-        alt: "",
-        width: 240,
-        description: " Duis rhoncus dui venenatis consequat porttitor",
-        title: "Aenean Sed Nibh A Magna Posuere",
-        link: "/",
-        linkText: "Link",
-        price: "$4.99"
-    }, {
-        src: Aenean_Sm,
-        alt: "",
-        width: 240,
-        description: " Duis rhoncus dui venenatis consequat porttitor",
-        title: "Aenean Sed Nibh A Magna Posuere",
-        link: "/",
-        linkText: "Link",
-        price: "$4.99"
-    }]
-];
 
 const Cart = (props) => {
+    const ItemCardData = callApi();
     return (
         <div  style={{maxWidth: "780px",paddingTop:123,paddingBottom:123,margin:"auto"}}>{
-            ItemCardData.map((itemGroup) => {
+            ItemCardData.map((itemGroup,key) => {
                 const items = itemGroup.map((data, key) => <ItemCard key={`${key}`} {...data} />)
-                return <div>{items}</div>
+                return <div key={`${key}`} >{items}</div>
             })}
         </div>
     );
---
+++ src/utils/index.js
@@ -0,0 +1,58 @@
+import Aenean_Sm from '../assets/Aenean_Sm.png';
+import Vestibulum from '../assets/Vestibulum_Sm.png';
+
+
+const ItemCardData = [
+                      [{
+                          src: Vestibulum,
+                          alt: "",
+                          width: 240,
+                          description: " Duis rhoncus dui venenatis consequat porttitor",
+                          title: "Aenean Sed Nibh A Magna Posuere",
+                          link: "/",
+                          linkText: "Link",
+                          price: "$4.99"
+                      }, {
+                          src: Aenean_Sm,
+                          alt: "",
+                          width: 240,
+                          description: " Duis rhoncus dui venenatis consequat porttitor",
+                          title: "Aenean Sed Nibh A Magna Posuere",
+                          link: "/",
+                          linkText: "Link",
+                          price: "$4.99"
+                      }, {
+                          src: Aenean_Sm,
+                          alt: "",
+                          width: 240,
+                          description: " Duis rhoncus dui venenatis consequat porttitor",
+                          title: "Aenean Sed Nibh A Magna Posuere",
+                          link: "/",
+                          linkText: "Link",
+                          price: "$4.99"
+                      }],
+                      [{
+                          src: Aenean_Sm,
+                          alt: "",
+                          width: 240,
+                          description: " Duis rhoncus dui venenatis consequat porttitor",
+                          title: "Aenean Sed Nibh A Magna Posuere",
+                          link: "/",
+                          linkText: "Link",
+                          price: "$4.99"
+                      }, {
+                          src: Aenean_Sm,
+                          alt: "",
+                          width: 240,
+                          description: " Duis rhoncus dui venenatis consequat porttitor",
+                          title: "Aenean Sed Nibh A Magna Posuere",
+                          link: "/",
+                          linkText: "Link",
+                          price: "$4.99"
+                      }]
+                  ];
+
+
+export const callApi = (params)=>{
+return ItemCardData;
+}
