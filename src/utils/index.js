2e6701cd29e6e9b8a7fb003d260846fd7e7fcd2c
shiv pratap singh <pratapshiv1992@gmail.com>
2019-10-05 11:29:23 +0530

    new item card images and content changed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,17 @@
 import Aenean_Sm from '../assets/Aenean_Sm.png';
-import Vestibulum from '../assets/Vestibulum_Sm.png';
+import Vestibulum_Sm from '../assets/Vestibulum_Sm.png';
+import Vestibulum_Comm_Sm from '../assets/Vestibulum_Comm_Sm.png';
 
 
 const ItemCardData = [
                       [{
-                          src: Vestibulum,
+                          src: Vestibulum_Sm,
                           alt: "",
                           width: 240,
-                          description: " Duis rhoncus dui venenatis consequat porttitor",
-                          title: "Aenean Sed Nibh A Magna Posuere",
+                          description: "Mauris imperdiet orci dapibus commodo libero nec, interib",
                           link: "/",
                           linkText: "Link",
-                          price: "$4.99"
+                          price: "$8.99"
                       }, {
                           src: Aenean_Sm,
                           alt: "",
@@ -22,14 +22,14 @@ const ItemCardData = [
                           linkText: "Link",
                           price: "$4.99"
                       }, {
-                          src: Aenean_Sm,
+                          src: Vestibulum_Comm_Sm,
                           alt: "",
                           width: 240,
-                          description: " Duis rhoncus dui venenatis consequat porttitor",
+                          description: "Mauris imperdiest orci dapibus commodo libero nec, interib",
                           title: "Aenean Sed Nibh A Magna Posuere",
                           link: "/",
                           linkText: "Link",
-                          price: "$4.99"
+                          price: "$6.99"
                       }],
                       [{
                           src: Aenean_Sm,
@@ -39,12 +39,12 @@ const ItemCardData = [
                           title: "Aenean Sed Nibh A Magna Posuere",
                           link: "/",
                           linkText: "Link",
-                          price: "$4.99"
+                          price: "$8.99"
                       }, {
                           src: Aenean_Sm,
                           alt: "",
                           width: 240,
-                          description: " Duis rhoncus dui venenatis consequat porttitor",
+                          description: "Pizza cheese burst garlic bread Mauris imperdiet orci dapibus",
                           title: "Aenean Sed Nibh A Magna Posuere",
                           link: "/",
                           linkText: "Link",
